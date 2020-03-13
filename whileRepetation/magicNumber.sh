47867b40fd71d3bc04870ffea928f77cde3b9f09
tushar8871 <tusharkhachane6@gmail.com>
2020-03-13 18:35:58 +0530

    Added : Find a number which user thinks

---
+++ whileRepetation/magicNumber.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+echo "Think number between 1 to 100"
+
+low=0;
+high=100;
+temp=0;
+
+while true
+do
+	mid=$((($low+$high)/2));
+	read -p  "Is Number less than (($low+$high)/2) than press 1 " a
+	if [ $low -lt $high ] && [ $a -eq 1 ]
+	then
+		high=$((($low+$high)/2));
+	elif [ $a -eq 2 ]
+	then
+		low=$mid;
+		high=$mid;
+	else
+		low=$((($low+$high)/2));
+	fi
+	
+	if [ $low -eq $high ]
+	then
+		echo "Found number which is : "$low
+		break;
+	fi
+done
