8d48e39101ce99e08cc383ac7685b0e414e347f9
tushar8871 <tusharkhachane6@gmail.com>
2020-03-13 15:17:00 +0530

    Added : compute Factors of a number N using prime factorization method

--- RepetationSelection/primeFactors.sh
+++ RepetationSelection/primeFactors.sh
@@ -5,9 +5,9 @@ read -p "Enter number : " number
 for (( i=2; $i<=$number; i++ ))
 do
 	count=0;
-	for (( j=1; $j<=$i; j++ ))
+	for (( j=1; j<=i; j++ ))
 	do
-		if [ $(($i%$j))==0 ] && [ $(($number%$i))==0 ]
+		if [ $(($i%$j)) -eq 0 ] && [ $(($number%$i)) -eq 0 ]
 		then
 			((count++))
 		fi
