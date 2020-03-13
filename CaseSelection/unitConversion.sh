def85d1e6263c2188ab4c9e7053c86074dcb7b46
tushar8871 <tusharkhachane6@gmail.com>
2020-03-13 12:30:28 +0530

    Added : Unit conversion problem

--- CaseSelection/unitConversion.sh
+++ CaseSelection/unitConversion.sh
@@ -15,22 +15,21 @@ case $choice in
 		inch=`echo " sacle=2; $feet/12" | bc -l`;
 		echo "$feet in : " $inch
 		;;
-   2)
-      read -p "Enter Inches : " inch
-      feet=`echo " sacle=2; $inch*12" | bc -l`;
-      echo "$inch in : " $feet
-      ;;
-   3)
-      read -p "Enter feets : " feet
-      metres=`echo " sacle=2; $feet*0.304" | bc -l`;
-      echo "$feet in : " $metres
-      ;;
-   4)
-      read -p "Enter metres : " metre
-      feet=`echo " sacle=2; $metre/0.304" | bc -l`;
-      echo "$feet in : " $feet
-      ;;
-
+	2)
+		read -p "Enter Inches : " inch
+		feet=`echo " sacle=2; $inch*12" | bc -l`;
+		echo "$inch in : " $feet
+		;;
+	3)
+		 read -p "Enter feets : " feet
+		 metres=`echo " sacle=2; $feet*0.304" | bc -l`;
+		 echo "$feet in : " $metres
+       ;;
+	4)
+		read -p "Enter metres : " metre
+		feet=`echo " sacle=2; $metre/0.304" | bc -l`;
+		echo "$feet in : " $feet
+		;;
 esac
 	
 
