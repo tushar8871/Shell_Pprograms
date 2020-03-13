8ae63dbad93940c5edda3cdc9cb3d05f0a6f6a86
tushar8871 <tusharkhachane6@gmail.com>
2020-03-13 11:49:12 +0530

    Added : Get Date from user and check it if its is present between march 20 and june 20

--- SelectionProblems/ValidateDayAndMethod.sh
+++ SelectionProblems/ValidateDayAndMethod.sh
@@ -9,9 +9,13 @@ year=`echo $date | awk -F/ '{print $3}'`;
 june=6
 march=3
 
-if [ $date1 -eq 20 ] && [ $month -le $june ] || [ $month -ge $march ]
+if [ $month -eq $march ] && [ $date1 -lt 20 ] 
 then
-	echo "Yes the $date and $month is present between March 20 and June 20" 
-else
 	echo "Not in between March 20 and June 20"
+elif [ $month -eq $june ] && [ $date1 -gt 20 ]
+then
+	echo "Not in between March 20 and June 20"
+elif [ $month -ge $march ] && [ $month -le $june ]
+then
+	echo "Yes the $date and $month is present between March 20 and June 20"
 fi
