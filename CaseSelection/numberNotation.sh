d3659c158b98a9a10e7ef99d2cd994040d5f0c97
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 09:02:27 +0530

    Added : Read a number from user and find its notation example unit , tens ,hudreds ,etc..

--- CaseSelection/numberNotation.sh
+++ CaseSelection/numberNotation.sh
@@ -1,32 +1,26 @@
 #!/bin/bash -x
 
-UNIT=1;
-TENS=10;
-HUNDREDS=100;
-THOUSANDS=1000;
-TENTHOUSANDS=10000;
-LAKH=100000;
 
 read -p "Enter number : " number
 
 case $number in
-	$UNIT)
+	1)
 		echo "Unit "
 		;;
-   $TENS)
-      echo "Tens "
-      ;;
-   $HUNDREDS)
-      echo "Hundred "
-      ;;
-   $THOUSANDS)
-      echo "Thousand "
-      ;;
-   $TENTHOUSANDS)
-      echo "Ten Thousand "
-      ;;
-   $LAKH)
-      echo "Lakh "
-      ;;
+   	10)
+      		echo "Tens "
+      		;;
+   	100)
+      		echo "Hundred "
+      		;;
+   	1000)
+      		echo "Thousand "
+      		;;
+   	10000)
+      		echo "Ten Thousand "
+      		;;
+   	100000)
+      		echo "Lakh "
+      		;;
 esac
 
