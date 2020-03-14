37cfc36b2582643cd8d1417fee922d1d4b7df2af
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 09:26:32 +0530

    Added : Get wallet amount and goal amount from user and start game upto gambler reached goal or his/her wallet get empty

--- whileRepetation/gambler.sh
+++ whileRepetation/gambler.sh
@@ -2,32 +2,31 @@
 
 read -p "Enter wallet amount : " walletAmount
 read -p "Enter goal amount : " GOAL
-read -p "Enter number of times you want to play : " numberTimes
 
 counter=0;
 i=0;
 tempWallet=$walletAmount;
 
-while [[ $i -lt $numberTimes ]]
+while true
 do
 	bet=$((RANDOM%2));
 	if [ $bet -eq 0 ]
 	then
-		walletAmount=$(($walletAmount-($tempWallet/2)));
+		walletAmount=$(($walletAmount-$bet));
 		((i++));
 	else
-		walletAmount=$(($walletAmount+($walletAmount/2)));
+		walletAmount=$(($walletAmount+$bet));
 		((counter++));
 		((i++));
 	fi
-done
 
-if [ $walletAmount<=0 ]
-then
-	echo "Gambler has lost ! "
-elif [ $GOAL -lt $walletAmount ]
-then
-	echo "Gambler has won ! $counter times wins And amount is  $walletAmount"
-else
-	echo "Gambler has won $counter times wins but not reach goal ! "
-fi
+	if [ $walletAmount -le 0 ]
+	then
+		echo "Gambler has lost ! "
+		break;
+	elif [ $GOAL -lt $walletAmount ]
+	then
+		echo "Gambler has won ! $counter times wins And amount is  $walletAmount"
+		break;
+	fi
+done
