5213c872908a9c9ef34b7e31dda55a88df5df9e0
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 12:57:25 +0530

    Refactor :Removed Unneccessary use of echo

--- SequenceProblems/UnitConversion.sh
+++ SequenceProblems/UnitConversion.sh
@@ -2,19 +2,14 @@
 
 #Feet to inch
 read -p "Enter feets to calculate inch : " feet
-inch=`echo "$feet/12" | bc -l`;
-echo "$feet in inch is :"  $inch
-
+inch=`echo "scale=2; $feet/12" | bc -l`;
 
 #rectangular plot in meters
 length=60.0
 breadth=40.0
 
-metres=`echo "$length*$breadth/3.28" | bc -l`;
-echo "Rectangular plot 60*40 in meters is :" $metres
-
+metres=`echo "scale=2 ;$length*$breadth/3.28" | bc -l`;
 
 #25 plots in acres 
-
-acres=`echo "$length*$breadth*25/43560" | bc -l`;
+acres=`echo "scale=2; $length*$breadth*25/43560" | bc -l`;
 echo "25 plots of 60*40 in acres is :" $acres 
