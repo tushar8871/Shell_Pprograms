a814b92fc7ff7c4ec0189068a7eb39fbb1a61e41
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 14:58:17 +0530

    Refactor : find a number which user thinks in his/her mind

--- whileRepetation/magicNumber.sh
+++ whileRepetation/magicNumber.sh
@@ -9,7 +9,7 @@ temp=0;
 while true
 do
 	mid=$((($low+$high)/2));
-	read -p  "Is Number less than (($low+$high)/2) than press 1 " a
+	read -p  "Is Number less than (($low+$high)/2) than press 1 and if mid is equal to your number than press 2 otherwise 0 " a
 	if [ $low -lt $high ] && [ $a -eq 1 ]
 	then
 		high=$((($low+$high)/2));
