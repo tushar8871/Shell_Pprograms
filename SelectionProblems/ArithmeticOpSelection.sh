29d0c1f3b5ac9ace8c2bad779f6cfd235bb2f6f5
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 12:46:16 +0530

    Refactor : Removed unneccessary echo

--- SelectionProblems/ArithmeticOpSelection.sh
+++ SelectionProblems/ArithmeticOpSelection.sh
@@ -5,16 +5,10 @@ read -p "Enter value 2 : " val2
 read -p "Enter value 3 : " val3
 
 echo "Expressions are : "
-echo "1. ($val1+$val2*$val3) is : "
+
 result1=$(($val1+$val2*$val3));
-
-echo "2. ($val3+$val1/$val2) is : "
 result2=$(($val3+$val1/$val2));
-
-echo "3. ($val1%$val2+$val3) is : "
 result3=$(($val1%$val2+$val3));
-
-echo "4. ($val1*$val2+$val3) is : "
 result4=$(($val1*$val2+$val3));
 
 max=0
@@ -31,8 +25,6 @@ else
 	max=$result4
 fi
 
-echo "Max is : " $max
-
 min=0
 if [ $result1 -lt $result2 ] && [ $result1 -lt $result3 ] && [ $result1 -lt $result4 ] 
 then
@@ -46,6 +38,3 @@ then
 else
    min=$result4
 fi
-
-echo "Minimum is : " $min
-
