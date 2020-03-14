00c987c1673b43707b4394ed70cb7678e81e8441
tushar8871 <tusharkhachane6@gmail.com>
2020-03-14 12:50:02 +0530

    Refactor : Chage constant in Uppercase format

--- SelectionProblems/ValidateDayAndMethod.sh
+++ SelectionProblems/ValidateDayAndMethod.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -x
 
+MARCH=3
+JUNE=6
+
 echo "Enter date : "
 read date
 month=`echo $date| awk -F/ '{print $1}'`;
 date1=`echo $date| awk -F/ '{print $2}'`;
 year=`echo $date | awk -F/ '{print $3}'`;
 
-june=6
-march=3
-
-if [ $month -eq $march ] && [ $date1 -lt 20 ] 
+if [ $month -le $MARCH ] && [ $date1 -lt 20 ] 
 then
 	echo "Not in between March 20 and June 20"
-elif [ $month -eq $june ] && [ $date1 -gt 20 ]
+elif [ $month -ge $JUNE ] && [ $date1 -gt 20 ]
 then
 	echo "Not in between March 20 and June 20"
-elif [ $month -ge $march ] && [ $month -le $june ]
+elif [ $month -ge $MARCH ] && [ $month -le $JUNE ]
 then
 	echo "Yes the $date and $month is present between March 20 and June 20"
 fi
