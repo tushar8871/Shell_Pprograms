986324d6804e92613f13ac48da0b409792996ab3
tushar8871 <tusharkhachane6@gmail.com>
2020-03-16 12:14:36 +0530

    Added : Roll a die and find number between 1-6 and store result in associative array and repeat till any one of number has reached 10 times and fimd the number that reached maximum and minimum times

---
+++ dictionary/diceRoll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+counter=0;
+array=( [0]=0 [1]=0 [2]=0 [3]=0 [4]=0 [5]=0 )
+
+while [[ ${array[$number]} -ne 10 ]]
+do
+	number=$((RANDOM%6));
+	array[$number]=`echo "$((${array[$number]}+1))" | bc`;
+	max=$number;
+done
+
+min=0;
+for (( val=0;val<6; val++ ))
+do
+	if [ ${array[$min]} -gt ${array[$val]} ]
+	then
+		min=$val
+	fi
+done
+
+echo "${array[@]}"
+echo "Maximum times come : $(($max+1))"
+echo "Minimum times come : $(($min+1))"
