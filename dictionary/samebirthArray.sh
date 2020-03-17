b677fd9a22a30e4f86b890f2819fe51c8050a41f
tushar8871 <tusharkhachane6@gmail.com>
2020-03-17 09:23:05 +0530

    Refactor : Changes made Instead of while and case loop placed for loop

--- dictionary/samebirthArray.sh
+++ dictionary/samebirthArray.sh
@@ -11,46 +11,8 @@ done
 
 echo "${birthArray[@]}"
 
-counter=0;
-while [[ $counter -ne 12 ]]
+
+for var in ${!birthArray[@]}
 do
-	case $counter in
-		0)
-			echo "January : ${birthArray[$counter]}"
-			;;
-		1)
-			echo "February : ${birthArray[$counter]}"
-			;;
-		2)
-			echo "March : ${birthArray[$counter]}"
-			;;
-		3)
-			echo "April : ${birthArray[$counter]}"
-			;;
-		4)
-			echo "May : ${birthArray[$counter]}"
-			;;
-		5)
-			echo "June : ${birthArray[$counter]}"
-			;;
-		6)
-			echo "July : ${birthArray[$counter]}"
-			;;
-		7)
-			echo "August : ${birthArray[$counter]}"
-			;;
-		8)
-			echo "September : ${birthArray[$counter]}"
-			;;
-		9)
-			echo "October : ${birthArray[$counter]}"
-			;;
-		10)
-			echo "November : ${birthArray[$counter]}"
-			;;
-		11)
-			echo "December : ${birthArray[$counter]}"
-			;;
-	esac
-	((counter++))
+	echo "$(($var+1)) : ${birthArray[$var]}"
 done
